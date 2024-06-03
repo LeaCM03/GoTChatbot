f1a33f86dd7332c2284f7512c3df2cbb62e63e8c
LeaCM03 <lcornelis@edu.tecnocampus.cat>
2024-06-03 12:11:05 +0200

    Problem fix attempt (unsuccessfull)

--- GoTbot/src/index.js
+++ GoTbot/src/index.js
@@ -1,1 +1,10 @@
-export const config = { defaultDelay: 0, defaultTyping: 0 }
+import { App } from '@botonic/react';
+import { routes } from './routes';
+import { locales } from './locales';
+
+const app = new App({
+  routes,
+  locales,
+});
+
+export default app;
--- GoTbot/src/routes.js
+++ GoTbot/src/routes.js
@@ -1,19 +1,14 @@
-import React from 'react'
-import { Text, Reply } from '@botonic/react'
+import React from 'react';
+import { Text } from '@botonic/react';
 
 export const routes = [
   {
-    path: 'initial',
-    text: /hi/i,
-    action: () => (
-      <>
-        <Text>Hello! Nice to meet you ;</Text>
-        <Text>
-          How can I help you?
-          <Reply payload='search'>Search product</Reply>
-          <Reply payload='track'>Track my order</Reply>
-        </Text>
-      </>
-    ),
+    path: 'start',
+    text: /^hi|hello|hey/i,
+    action: () => <Text>Hi! Welcome to the Game of Thrones bot.</Text>,
   },
-]
+  {
+    path: 'default',
+    action: () => <Text>Sorry, I didn't understand that.</Text>,
+  },
+];
